e66a77304808aeafb86bae0421fadb80d314eb07
Capelaum <thecapellett@gmail.com>
2021-02-01 04:23:32 -0300

    redirect succes!

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,12 +1,10 @@
 // import { getRouteMatcher } from 'next/dist/next-server/lib/router/utils';
 import React from 'react';
-
 import { ThemeProvider } from 'styled-components';
 import QuizScreen from '../../src/screens/Quiz';
 
 export default function QuizDaGaleraPage({ externalDB }) {
-
-  console.log(externalDB);
+  // console.log(externalDB);
   return (
     <ThemeProvider theme={externalDB.theme}>
       <QuizScreen
@@ -23,55 +21,27 @@ export default function QuizDaGaleraPage({ externalDB }) {
 
 export async function getServerSideProps(context) {
   // console.log('Infos que o next passa:', context.query);
-  const [githubUser, projectName] = context.query.id.split('___');
-
+  const [githubUser, projectName] = context.query.id.split('_');
   try { 
-    // const externalDB = await fetch('https://aluraquiz-css.omariosouto.vercel.app/api/db')
     const externalDB = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
       .then(response => {
         if(response.ok) {
           return response.json();
         }
         throw new Error('Falha em pegar os dados');
-      }) 
+      })
       .then(responseObject => responseObject)
-    
-      console.log('externalDB:', externalDB);// resposta do lado do servidor
-
-      // retorna a propspara o componente
+      // retorna a props para o componente
       return {
         props: { externalDB },
       };
   } catch (error) {
     console.error(error);
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/'
+      }
+    };
   }
 }
-
-// export async function getServerSideProps(context) {
-//   const [projectName, githubUser] = context.query.id.split('___');
-
-//   try {
-//     const dbExterno = await fetch(`https://aluraquiz-css.omariosouto.vercel.app/api/db`)
-//     // const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
-//       .then((respostaDoServer) => {
-//         if (respostaDoServer.ok) {
-//           return respostaDoServer.json();
-//         }
-//         throw new Error('Falha em pegar os dados');
-//       })
-//       .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
-//       // .catch((err) => {
-//       //   console.error(err);
-//       // });
-
-//     console.log('dbExterno', dbExterno);
-//     console.log('Infos que o Next da para nós', context.query.id);
-//     return {
-//       props: {
-//         dbExterno,
-//       },
-//     };
-//   } catch(err) {
-//     throw new Error(err);
-//   }
-// }
